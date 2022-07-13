cd558676e6f6b235af7aed89afa4d53df1d8b564
Tawanda3012 <tawanda@uncommon.org>
2022-07-13 09:41:06 +0200

    new

---
+++ src/Nav/Nav.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import "../Nav/Nav.css";
+import { SidebarData } from "../Nav/SidebarData";
+import { Link } from "react-router-dom";
+import background from "../img/banner.png";
+import logo from "../img/logo.png";
+
+function Nav() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+  return (
+    <>
+      <div
+        className="header"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundRepeat: "no-repeat",
+          height: "100vh",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="logo">
+          <img src={logo} alt="logo" className="logo-img" />
+        </div>
+        <div className="menu-bars1" onClick={showSidebar}>
+          <div className="one"></div>
+          <div className="two"></div>
+          <div className="three"></div>
+        </div>
+      </div>
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items" onClick={showSidebar}>
+          <li className="navbar-toggle">
+            <div className="menu-bars">
+              <AiOutlineClose />
+            </div>
+          </li>
+          {SidebarData.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Nav;
---
+++ src/Nav/SidebarData.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+
+
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        cName: 'nav-text'
+    },
+    {
+        title: 'About-Us',
+        path: '/about-us',
+        cName: 'nav-text'
+    },
+    {
+        title: 'Events',
+        path: '/events',
+        cName: 'nav-text'
+    },
+    {
+        title: 'News & Updates',
+        path: '/news & updates',
+        cName: 'nav-text'
+    },
+    {
+        title: 'Upcoming Events',
+        path: '/upcoming events',
+        cName: 'nav-text'
+    },
+    {
+        title: 'Contact Us',
+        path: '/contact us',
+        cName: 'nav-text'
+    },
+
+]
