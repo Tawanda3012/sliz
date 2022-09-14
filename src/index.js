1a91d7d282d7e3353f28b260016d643b9fb3fef1
Tawanda3012 <tawanda@uncommon.org>
2022-09-14 12:59:59 +0200

    new

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import SimpleReactLightbox from 'simple-react-lightbox';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   
+    <SimpleReactLightbox>
 			<App />
-		
+		</SimpleReactLightbox>
   </React.StrictMode>
 );
 
