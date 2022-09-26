c1fdb53c1c3eead16772e4b01a96d3c45ad5a88d
Tawanda3012 <tawanda@uncommon.org>
2022-09-26 11:54:05 +0200

    new

---
+++ build/precache-manifest.0961305c28b2fc2bb41e0f95378ef291.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "70e132ff22cd418f81e3",
+    "url": "/static/css/main.0cbfb4f9.chunk.css"
+  },
+  {
+    "revision": "70e132ff22cd418f81e3",
+    "url": "/static/js/main.70e132ff.chunk.js"
+  },
+  {
+    "revision": "7984912c93713be75d82",
+    "url": "/static/js/1.7984912c.chunk.js"
+  },
+  {
+    "revision": "789e919f25dc5fce3f6f",
+    "url": "/static/css/2.0a7bffa2.chunk.css"
+  },
+  {
+    "revision": "789e919f25dc5fce3f6f",
+    "url": "/static/js/2.789e919f.chunk.js"
+  },
+  {
+    "revision": "32438f8c8e636a7d1a5d",
+    "url": "/static/js/runtime~main.32438f8c.js"
+  },
+  {
+    "revision": "e760c65116e4eaf200b98463e9514074",
+    "url": "/static/media/Group 9.e760c651.png"
+  },
+  {
+    "revision": "c1efff0dfa0d17f08c7eb611b83837ec",
+    "url": "/static/media/Rectangle 179.c1efff0d.png"
+  },
+  {
+    "revision": "26dab45a1ef0664f44520c9eeec2b7c0",
+    "url": "/static/media/Rectangle 159.26dab45a.png"
+  },
+  {
+    "revision": "f987d2cd8fed7315c0678bfde3a78a81",
+    "url": "/static/media/Rectangle 160.f987d2cd.png"
+  },
+  {
+    "revision": "8ba59bd421692a0f26ab68ef78796878",
+    "url": "/static/media/Rectangle 256.8ba59bd4.png"
+  },
+  {
+    "revision": "5bff8e555b800fc75d7bb65be2a9deb2",
+    "url": "/static/media/Rectangle 176.5bff8e55.png"
+  },
+  {
+    "revision": "959d3783a8f2ba125b5898b09e02b961",
+    "url": "/static/media/logo.959d3783.png"
+  },
+  {
+    "revision": "d43744fdbc85f0683fcbdab30c5279d4",
+    "url": "/static/media/Rectangle141.d43744fd.png"
+  },
+  {
+    "revision": "4d697ab3b06cabcb1e1b6941db3acc21",
+    "url": "/static/media/Rectangle 311.4d697ab3.png"
+  },
+  {
+    "revision": "2a229c5a0ba003c27a9c48f960667bfe",
+    "url": "/static/media/Bannerimg3.2a229c5a.png"
+  },
+  {
+    "revision": "7186d58462343f8ee122d0c6bfac7895",
+    "url": "/static/media/Rectangle 328.7186d584.png"
+  },
+  {
+    "revision": "95edd5e8bad90aa095b431447af10d9f",
+    "url": "/static/media/Rectangle 165.95edd5e8.png"
+  },
+  {
+    "revision": "1b072ea9776291aa03aee51478dec682",
+    "url": "/static/media/Banner img2.1b072ea9.png"
+  },
+  {
+    "revision": "6a988b9e7e9e8f117de7980a6f4c1e90",
+    "url": "/static/media/banner.6a988b9e.png"
+  },
+  {
+    "revision": "51144f5e3f4083b19fea3a5f28877d12",
+    "url": "/static/media/Banner img4.51144f5e.png"
+  },
+  {
+    "revision": "f6a7f1ed26f00e7a212bf20a4e526a6c",
+    "url": "/static/media/Banner img1.f6a7f1ed.png"
+  },
+  {
+    "revision": "5740909d4ad7d3ea9ac73d5b5b0ba714",
+    "url": "/static/media/image 12.5740909d.png"
+  },
+  {
+    "revision": "c85aa96ffb45ac5b05d14bb3b1fcddc4",
+    "url": "/static/media/Rectangle 248.c85aa96f.png"
+  },
+  {
+    "revision": "961fe20661b57acd8ec07035e4891f8b",
+    "url": "/static/media/image 5.961fe206.png"
+  },
+  {
+    "revision": "8d0be58ef37346dcab0f45c3e0e80584",
+    "url": "/static/media/image 4.8d0be58e.png"
+  },
+  {
+    "revision": "ac56efc23e0912196eda9cfba228b621",
+    "url": "/static/media/image 8.ac56efc2.png"
+  },
+  {
+    "revision": "878500242fe415933513c2635b28f8be",
+    "url": "/static/media/image 7.87850024.png"
+  },
+  {
+    "revision": "8e4f26354acfadd73fddebafd0051bb7",
+    "url": "/static/media/image 11.8e4f2635.png"
+  },
+  {
+    "revision": "2ba1fecd5fc4638c266dda7d73f3592d",
+    "url": "/static/media/17795995_273698123057881_8604271747876962932_n 1.2ba1fecd.png"
+  },
+  {
+    "revision": "e8e75e6e67919e25583227f147094d94",
+    "url": "/static/media/Rectangle 302.e8e75e6e.png"
+  },
+  {
+    "revision": "7ddb207cd4aa8bf227fa3c7068ea607d",
+    "url": "/index.html"
+  }
+];
